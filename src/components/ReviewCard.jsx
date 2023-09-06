b681787d483b8b756967024a088eb6e24f5cbb60
NIYATI GUPTA <niyatigupta@Maccy.local>
2023-09-06 18:52:13 +0530

    first commit

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { star } from '../assets/icons'
+
+const ReviewCard = ({imgURL, customerName, rating, feedback}) => {
+  return (
+    <div className='flex flex-col justify-center items-center rounded-2xl hover:border-2'>
+        <img src={imgURL} alt='customer' className='rounded-full object-cover w-[120px] h-[120px] mt-2'/>
+        <p className='mt-6 max-w-sm text-center info-text'>{feedback}</p>
+        <div className='mt-3 flex justify-center items-center gap-2.5'>
+            <img src={star} width={24} height={24} className='object-contain m-0'/>
+            <p className='text-xl font-montserrat text-slate-gray'>({rating})</p>
+        </div>
+        <h3 className='mt-1 font-palanquin text-3xl text-center font-bold'>{customerName}</h3>
+    </div>  
+  )
+}
+
+export default ReviewCard
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Footer from './Footer'
+import CustomerReviews from './CustomerReviews'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import SpecialOffer from './SpecialOffer'
+
+export{
+    Hero, 
+    PopularProducts,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+    Footer
+}
